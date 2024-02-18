12cf6bcfc46e6bdabc19215ef453d91759c02ef6
JamorMoussa <moussajamorsup@gmail.com>
2024-02-18 07:22:38 +0100

    make some changes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+from home import home_page
+from demo import demo_page 
+
 def main():
     st.set_page_config(page_title="AniThing", page_icon=":magic_wand", layout="wide")
     st.title("AniThing : GenAI Solution ")
@@ -11,30 +14,11 @@ def main():
 
     # Home Page Content
     if page == "Home":
-        st.image('./media/logo2.png', width=300)
-        st.header("Welcome to AniThing - Animate Anything")
-        st.write("""
-            AniThing is a Manim code generator tool that allows users to input prompts and automatically generate Manim animation. 
-            Simply enter your prompt in the text area provided in the 'Demo' section and click the 'Generate Manim Code' button to see the code.
-            Explore the different features and options available in the sidebar navigation.
-        """)
+        home_page()
 
     # Demo Page Content
     elif page == "Demo":
-        st.write("""
-            ## Demo Area
-            Enter your prompt below to generate Manim animation code:
-        """)
-        # Input prompt text area
-        prompt = st.text_area("Enter your prompt here:")
-        # Button to generate Manim code
-        if st.button("Generate Manim Code"):
-            # Call function to generate Manim code (to be implemented)
-            # manim_code = generate_manim_code(prompt)
-            # Display the generated Manim code
-            st.header("Sample Video")
-            video_path = "./media/videos/AreaUnderCurveExample.mp4"
-            st.video(video_path)
+        demo_page()
 
 if __name__ == "__main__":
     main()
